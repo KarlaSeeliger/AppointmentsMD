8124ed86063d2a25a9d05a6da84d4f23f4e87c53
Karla Seeliger <59029021+KarlaSeeliger@users.noreply.github.com>
2020-02-21 12:14:12 -0600

    update

--- Backend/app.js
+++ Backend/app.js
@@ -11,7 +11,7 @@ var cors = require('cors');
 app.use(cors());
 
 
-mongoose.connect("mongodb+srv://Karla:seeliger@cluster0-ghuir.mongodb.net/appointmentsMD?retryWrites=true&w=majority").then(()=>{
+mongoose.connect("mongodb+srv://Karla:pswd@cluster0-ghuir.mongodb.net/appointmentsMD?retryWrites=true&w=majority").then(()=>{
     console.log("connected");
 })
 .catch(()=>{console.log("failed")})
